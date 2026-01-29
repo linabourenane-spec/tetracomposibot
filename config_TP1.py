a1f53372712709547a9700225907e58c24f67830
bourenane lina <lina.bourenane@etu.sorbonne-universite.fr>
2026-01-29 22:09:20 +0100

    tp1

--- config_TP1.py
+++ config_TP1.py
@@ -21,10 +21,14 @@ display_time_stats = True
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
 import robot_dumb
+import robot_braitenberg_avoider
+import robot_braitenberg_loveWall
+import robot_braitenberg_hateWall
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+
+    robots.append(robot_braitenberg_avoider.Robot_player(50, y_center, 0, name="My Robot", team="A"))
     return robots
